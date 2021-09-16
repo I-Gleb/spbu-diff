b83b35247feff7a83443d6378c407cea3d6a99d8
Gleb Ingman <igr2309@mail.ru>
2021-09-17 01:15:50 +0300

    Made a prototype

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,39 @@
-fun main(args: Array<String>) {
+/*
+ * Получает параметры командной строки. При необходимости запрашивает данные у пользователя.
+ * Производится считываение и обработка входных даныых.
+ * Функция возращает два сравниваемых файла.
+ */
+fun processInput(args: Array<String>): Pair<List<String>, List<String> > {
     TODO()
+}
+
+
+/*
+ * Получает два файла
+ * Файлы сравниваются.
+ * Функция возращает файл-сравнение, в котором у каждой строки есть пометка о её статусе
+ */
+fun findChanges(originalFile : List<String>, updatedFile : List<String>): List<Pair<Int, String> > {
+    TODO()
+}
+
+
+/*
+ * Получает файл-сравнение.
+ * Выводит разницу между файлами по файлу-сравнению.
+ */
+fun printDifference(comparisonFile : List<Pair<Int, String> >) {
+    TODO()
+}
+
+
+fun main(args: Array<String>) {
+    // получили входные файлы
+    val (originalFile, updatedFile) = processInput(args)
+
+    // постоили по ним файл-сравнение
+    val comparisonFile = findChanges(originalFile, updatedFile)
+
+    // вывели отчёт об изменениях между файлами
+    printDifference(comparisonFile)
 }
